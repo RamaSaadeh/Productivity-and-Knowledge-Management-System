24b3323ff1a97419f2fe328d9dbc4112ec1b9218
Ubxyy <151298854+Ubxyy@users.noreply.github.com>
2024-02-08 16:08:09 +0000

    Add files via upload
    Test to see if php connects to database

---
+++ test_connection.php
@@ -0,0 +1,16 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+$servername = "1";
+$username = "host";
+$password = "Team017FTW!"; // Replace with the actual password
+$dbname = "makeitall";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
