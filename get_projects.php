1c8e9fc5bc8c661c065d118a06c40658a9cbd1fa
CSparrow-22 <152895548+CSparrow-22@users.noreply.github.com>
2024-02-12 20:17:38 +0000

    Redundant code removed
    Had some debugging lines still in the file- so removed these

--- get_projects.php
+++ get_projects.php
@@ -1,20 +1,11 @@
 <?php
 
-echo "Database Connection";
-
 $servername = "localhost";
 $username = "team017";
 $password = "xngk4RgUqJxMjKX3EMak";
 $database = "team017";
 
 $conn = new mysqli($servername, $username, $password, $database);
-
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
-
-
 
 // SQL query to fetch options from the database
 $sql = "SELECT project_id, proj_name FROM `projects`;";
