9b1d0f0c03eb5bcc29d7d19ca2da2f12559dc94f
Haamid <haamidujillani@gmail.com>
2024-02-15 19:14:24 +0000

    Edit and delete feature is now only accessible if the post was created by the user currently logged in

--- fetch-single-post.php
+++ fetch-single-post.php
@@ -4,28 +4,42 @@ header('Content-Type: application/json');
 
 include 'db.php';
 
+//hardcoded user ID for testing purposes
+$userId = 1;
+
+
 //check if the Post ID is in the request
 if (isset($_GET['id'])) {
     //sanitize the input to prevent SQL injection
     $postId = mysqli_real_escape_string($conn, $_GET['id']);
     
     //query to retrieve the details of the specific post based on its ID
-    $sql = "SELECT p.PostID, p.Title, p.Content, p.DateCreated, p.DatePublished, p.IsDraft, p.LikesCount, p.Topic, u.name AS AuthorName
+    $sql = "SELECT p.PostID, p.Title, p.Content, p.DateCreated, p.DatePublished, p.IsDraft, p.LikesCount, p.Topic, u.name AS AuthorName,
+            (p.UserID = $userId) AS IsUserOwner
             FROM Posts p
             INNER JOIN users u ON p.UserID = u.user_id
             WHERE p.PostID = '$postId'";
 
+
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
-        $post = $result->fetch_assoc(); // Fetch the post details
-        echo json_encode($post); // Encode the post details as JSON and output it
+        $post = $result->fetch_assoc(); //fetch the post details
+        //prepare the response
+        $response = [
+            'success' => true,
+            'data' => $post,
+            'isUserOwner' => (bool)$post['IsUserOwner'] //cas isUserOwner to bool value as can only be 1 or 0
+        ];
+        
+        echo json_encode($response); //encode response as JSON
     } else {
-        echo json_encode(['error' => 'Post not found']); // Return an error if the post is not found
+        echo json_encode(['success' => false, 'error' => 'Post not found']); //if post not found, return error
     }
 } else {
-    echo json_encode(['error' => 'Post ID not provided']); // Return an error if the Post ID is not provided
+    echo json_encode(['success' => false, 'error' => 'Post ID not provided']); //if post id not provided from url, return an error
 }
+
 
 $conn->close();
 ?>
