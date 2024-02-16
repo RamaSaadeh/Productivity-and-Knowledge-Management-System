3cf28f233e11b7b7b189d7d64e956290473c915c
mjtranter <mjtranter2003@outlook.com>
2024-02-16 14:43:09 +0000

    Added team leader session storage

--- login.php
+++ login.php
@@ -26,6 +26,18 @@
 		$stmt->bind_result($user_id2, $role, $email);
 		$stmt->fetch();
 		$stmt->close();
-		echo "$user_id2/$role/$email";
+		$stmt = $conn->prepare("SELECT leader_id FROM projects");
+		$stmt->execute();
+		$stmt->bind_result($leaders);
+		$leader = false;
+		while ($stmt->fetch()) {
+			if ($leaders == $user_id2) {
+				$role = "Leader";
+				$leader = true;
+				echo "$user_id2/$role/$email";
+			}
+		}
+		$stmt->close();
+		if (!$leader) echo "$user_id2/$role/$email";
 	}
 ?>
