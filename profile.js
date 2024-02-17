8d4d24872828660d947b644af804c7b7ef026e1f
mjtranter <mjtranter2003@outlook.com>
2024-02-17 22:10:50 +0000

    moving js to html

--- profile.js
+++ profile.js
@@ -122,30 +122,3 @@ function passwordComplete() {
     document.getElementById("confirm").style.borderColor = colour;
     return false;
 }
-
-var inviteForm = document.getElementById("inviteuseropaquebg");
-
-function openForm() {
-    inviteForm.style.display = "block";
-}
-
-function closeForm() {
-    inviteForm.style.display = "none";
-    document.getElementById("email").value = "";
-    document.getElementById("emailError").style.display = "none";
-}
-
-function sendInvite() {
-    var email = document.getElementById("email").value;
-    var label = document.getElementById("emailError");
-    if (email.slice(email.length - 18).toLowerCase() != "@make-it-all.co.uk") {
-        label.innerHTML = "Email address is not valid";
-        label.style.color = "red";
-        label.style.display = "block";
-    }
-    else {
-        label.innerHTML = "Invite sent!";
-        label.style.color = "#2fe617";
-        label.style.display = "block";
-    }
-}
