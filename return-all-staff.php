8570d716464ec32bcf797f94e214a6d8aae0f833
motsodumade <motsodumade@gmail.com>
2024-02-17 18:51:11 +0000

    echoing staffData json array

--- return-all-staff.php
+++ return-all-staff.php
@@ -10,28 +10,60 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+ // Set the appropriate headers to indicate JSON content
+ header('Content-Type: application/json');
+
 // Execute SQL queries to fetch users data
-$sql = "SELECT user_id, username, email, role FROM users;";
-$result = $conn->query($sql);
+$sql1 = "SELECT user_id, name, email, role FROM users;";
+$sql2 = "SELECT project_id, manager_id, leader_id FROM projects;";
+$result1 = $conn->query($sql1);
+$result2 = $conn -> query($sql2);
 
-if ($result->num_rows > 0) {
+$staffData = array();
+$teamsData = array();
+
+if ($result1->num_rows > 0) {
     // Fetch the results into an associative array
-    $usersData = array();
-    while($row = $result->fetch_assoc()) {
-        $usersData[] = $row;
+    while($row = $result1->fetch_assoc()) {
+        // append data to staffData array
+        $staffData[] = $row;
     }
-
-    // Format the results into JSON format
-    $jsonResponse = json_encode($usersData);
-
-    // Set the appropriate headers to indicate JSON content
-    header('Content-Type: application/json');
-
-    // Output the JSON response
-    echo $jsonResponse;
 } else {
     echo "No users found";
 }
+
+if ($result2 -> num_rows > 0){
+    while ($row = $result2 -> fetch_assoc()){
+        $teamsData[] = $row;
+    }
+}
+
+// add team membership info to each associative array in staffData
+foreach($staffData as $user){
+    $managing = array();
+    $leading = array();
+    foreach ($teamsData as $team){
+        if ($team['manager_id'] == $user['user_id']){
+            $managing[] = $team['project_id'];
+        }
+        if($team['leader_id'] == $user['user_id']){
+            $leading[] = $team['project_id'];
+        }
+    }
+    $user['managing'] = $managing;
+    $user['leading'] = $leading;
+}
+
+
+// Format the results into JSON format
+$jsonResponse = json_encode($staffData);
+
+
+
+// Output the JSON response
+echo $jsonResponse;
+
+
 
 // Close the database connection
 $conn->close();
