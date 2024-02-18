9d90703d9f777c383b3afc1aa5d3f562f4b34c0a
CSparrow-22 <152895548+CSparrow-22@users.noreply.github.com>
2024-02-18 23:45:17 +0000

    Add files via upload

--- teamleaderdash.js
+++ teamleaderdash.js
@@ -314,78 +314,78 @@ $.ajax({
 //   document.getElementById("changeroleopaquebg").style.display = "none";
 // }
 
-function closechangeroleForm() {
-  document.getElementById("changeroleopaquebg").style.display = "none";
-}
+// function closechangeroleForm() {
+//   document.getElementById("changeroleopaquebg").style.display = "none";
+// }
 
-function openremovestaffForm(){
-  $.ajax({
-    type: "POST",
-    url: "loadstaff_in_proj.php",
-    data: {
-      ID: selectedProjectID
-    },
-    success: function (response) {
-      if (response === "invalid") {
-        alert("Something went wrong");
-      } else {	
+// function openremovestaffForm(){
+//   $.ajax({
+//     type: "POST",
+//     url: "loadstaff_in_proj.php",
+//     data: {
+//       ID: selectedProjectID
+//     },
+//     success: function (response) {
+//       if (response === "invalid") {
+//         alert("Something went wrong");
+//       } else {	
 
-        var allStaff = JSON.parse(response);
+//         var allStaff = JSON.parse(response);
 
-        var selectDropdown = document.getElementById("select_removestaff");
-        //we want to overwrite the select/reset it each time so we dont just add to what was already there
-        selectDropdown.innerHTML = '<option value="" disabled selected>Select staff member to add to project</option>';
+//         var selectDropdown = document.getElementById("select_removestaff");
+//         //we want to overwrite the select/reset it each time so we dont just add to what was already there
+//         selectDropdown.innerHTML = '<option value="" disabled selected>Select staff member to add to project</option>';
 
 
-        for (var staffrow in allStaff) { //for every staff returned, add each to the table
+//         for (var staffrow in allStaff) { //for every staff returned, add each to the table
 
-          var option = document.createElement("option");
+//           var option = document.createElement("option");
 
-            // Set the value of the option to user_id
-            option.value = allStaff[staffrow][0];
+//             // Set the value of the option to user_id
+//             option.value = allStaff[staffrow][0];
             
-            // Concatenate name and email and set it as the text of the option
-            option.text = "#" + allStaff[staffrow][0] + "        |         " + allStaff[staffrow][1] + "         |         " + allStaff[staffrow][2];
+//             // Concatenate name and email and set it as the text of the option
+//             option.text = "#" + allStaff[staffrow][0] + "        |         " + allStaff[staffrow][1] + "         |         " + allStaff[staffrow][2];
             
-            selectDropdown.appendChild(option);
-        }
-      }
-    }
-  });
+//             selectDropdown.appendChild(option);
+//         }
+//       }
+//     }
+//   });
 
-// now staff not in the team are loaded into the <select> we are going to open the form
- document.getElementById("removestaffopaquebg").style.display = "block";
-}
+// // now staff not in the team are loaded into the <select> we are going to open the form
+//  document.getElementById("removestaffopaquebg").style.display = "block";
+// }
 
-function remove_fromteam(){
+// function remove_fromteam(){
 
-  event.preventDefault();
+//   event.preventDefault();
 
-  var selectedUserID = document.getElementById("select_removestaff").value;
+//   var selectedUserID = document.getElementById("select_removestaff").value;
 
-  $.ajax({
-    type: "POST",
-    url: "remove_teammember.php",
-    data: {
-      projectID: selectedProjectID,
-      userID: selectedUserID
-    },
-    success: function (response) {
-      if (response === "invalid") {
-        alert("Something went wrong");
-      } 
+//   $.ajax({
+//     type: "POST",
+//     url: "remove_teammember.php",
+//     data: {
+//       projectID: selectedProjectID,
+//       userID: selectedUserID
+//     },
+//     success: function (response) {
+//       if (response === "invalid") {
+//         alert("Something went wrong");
+//       } 
 
-      window.location.href = "managerdash.html?selected_project_ID="+selectedProjectID;
-    }
-  });
+//       window.location.href = "managerdash.html?selected_project_ID="+selectedProjectID;
+//     }
+//   });
 
-  // now staff not in the team are loaded into the <select> we are going to open the form
-  document.getElementById("removestaffopaquebg").style.display = "none";
-}
+//   // now staff not in the team are loaded into the <select> we are going to open the form
+//   document.getElementById("removestaffopaquebg").style.display = "none";
+// }
 
-function closeremovestaffForm() {
-  document.getElementById("removestaffopaquebg").style.display = "none";
-}
+// function closeremovestaffForm() {
+//   document.getElementById("removestaffopaquebg").style.display = "none";
+// }
 
 function openaddtaskForm() {
 
