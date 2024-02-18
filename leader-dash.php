86dfaa65c4d4ce5dde9f0be1c92ed20a77d27730
mjtranter <mjtranter2003@outlook.com>
2024-02-18 20:53:22 +0000

    changing css

--- leader-dash.php
+++ leader-dash.php
@@ -18,7 +18,7 @@
 
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css">
-  <link href="accessproject.css" rel="stylesheet"></script>
+  <link href="leader-dash.css" rel="stylesheet"></script>
 </head>
 
 <body onload="checkLogin()">
