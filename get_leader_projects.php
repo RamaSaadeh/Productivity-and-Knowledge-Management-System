7ed738fc76d368ed168f5a45a97043492100b402
mjtranter <mjtranter2003@outlook.com>
2024-02-18 21:05:56 +0000

    gets only leader projects

--- get_leader_projects.php
+++ get_leader_projects.php
@@ -14,13 +14,14 @@ $conn = new mysqli($servername, $username, $dbpassword, $database);
 $sql = "SELECT project_id, proj_name FROM `projects` WHERE leader_id = $id;";
 $result = $conn->query($sql);
 
-
+$array = array();
 // Generate values for each option in the dropdown
 while ($row = $result->fetch_assoc()) {
-    echo '<option value="' . $row['project_id'] . '">' . $row['project_id'] .': '. $row['proj_name'] . '</option>';
+    array_push($array,`'<option value="' . $row['project_id'] . '">' . $row['project_id'] .': '. $row['proj_name'] . '</option>'`);
 }
 
-echo '</select>';
+array_push($array,"'</select>'");
+echo json_encode($array);
 
 $conn->close();
 ?>
