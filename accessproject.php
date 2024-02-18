2c09e948a3f5b94d9bed8cdda34bbeeea456ea83
mjtranter <mjtranter2003@outlook.com>
2024-02-18 02:44:02 +0000

    test fix accessproject

--- accessproject.php
+++ accessproject.php
@@ -136,34 +136,6 @@
 	</div>
 </footer>
 
-<script>
-		var inviteForm = document.getElementById("inviteuseropaquebg");
-
-        function openForm() {
-            inviteForm.style.display = "block";
-        }
-
-        function closeForm() {
-            inviteForm.style.display = "none";
-            document.getElementById("email").value = "";
-            document.getElementById("emailError").style.display = "none";
-        }
-
-        function sendInvite() {
-            var email = document.getElementById("email").value;
-            var label = document.getElementById("emailError");
-            if (email.slice(email.length - 18).toLowerCase() != "@make-it-all.co.uk") {
-                label.innerHTML = "Email address is not valid";
-                label.style.color = "red";
-                label.style.display = "block";
-            }
-            else {
-                label.innerHTML = "Invite sent!";
-                label.style.color = "#2fe617";
-                label.style.display = "block";
-            }
-        }
-</script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/js/bootstrap.min.js"></script>
 <script src="accessproject.js"></script>
 	
