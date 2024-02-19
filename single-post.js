cda5e08c529ecf4d7d393b7463631296c7b33abc
Haamid <haamidujillani@gmail.com>
2024-02-19 00:00:46 +0000

    Comments are now updated by hour/minute/second

--- single-post.js
+++ single-post.js
@@ -1075,7 +1075,7 @@ document.addEventListener("DOMContentLoaded", function () {
 		
 		//function for getting the date
 	function extractDateFromElement(element) {
-		const regex = /(\w+ \d+, \d{4})/;  
+		const regex = /(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})/;
 		const match = regex.exec(element.textContent);
 		return match ? match[1] : null; 
 	}
