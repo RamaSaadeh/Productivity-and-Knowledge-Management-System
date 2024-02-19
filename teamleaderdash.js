750762a3fe7ee08a5dca3bac49ae967349b61122
CSparrow-22 <152895548+CSparrow-22@users.noreply.github.com>
2024-02-19 00:24:57 +0000

    Add files via upload

--- teamleaderdash.js
+++ teamleaderdash.js
@@ -236,7 +236,7 @@ function addstaff_toteam(){
         alert("Something went wrong");
       } 
 
-      window.location.href = "managerdash.html?selected_project_ID="+selectedProjectID;
+      window.location.href = "teamleaderdash.html?selected_project_ID="+selectedProjectID;
     }
   });
 
@@ -306,7 +306,7 @@ function maketeamleader(){
         alert("Something went wrong");
       } 
 
-      window.location.href = "managerdash.html?selected_project_ID="+selectedProjectID;
+      window.location.href = "teamleaderdash.html?selected_project_ID="+selectedProjectID;
     }
   });
 
@@ -375,7 +375,7 @@ function remove_fromteam(){
         alert("Something went wrong");
       } 
 
-      window.location.href = "managerdash.html?selected_project_ID="+selectedProjectID;
+      window.location.href = "teamleaderdash.html?selected_project_ID="+selectedProjectID;
     }
   });
 
@@ -519,7 +519,7 @@ function addnewtask() {
         alert("Something went wrong");
       } 
 
-      window.location.href = "managerdash.html?selected_project_ID="+selectedProjectID;
+      window.location.href = "teamleaderdash.html?selected_project_ID="+selectedProjectID;
     }
   });
 
@@ -590,7 +590,7 @@ function updatetasknotes() {
               // Hide the task notes form
               document.getElementById("tasknotesopaquebg").style.display = "none";
               // Redirect to the manager dashboard
-              window.location.href = "managerdash.html?selected_project_ID=" + selectedProjectID;
+              window.location.href = "teamleaderdash.html?selected_project_ID=" + selectedProjectID;
           }
         },
       });
@@ -695,7 +695,7 @@ function save_changesto_task(){
           } else {
               
               // Redirect to the manager dashboard
-              window.location.href = "managerdash.html?selected_project_ID=" + selectedProjectID;
+              window.location.href = "teamleaderdash.html?selected_project_ID=" + selectedProjectID;
 
           }
         },
@@ -725,7 +725,7 @@ function delete_task(){
               // Hide the task notes form
               document.getElementById("edittaskopaquebg").style.display = "none";
               // Redirect to the manager dashboard
-              window.location.href = "managerdash.html?selected_project_ID=" + selectedProjectID;
+              window.location.href = "teamleaderdash.html?selected_project_ID=" + selectedProjectID;
           }
         },
       });
