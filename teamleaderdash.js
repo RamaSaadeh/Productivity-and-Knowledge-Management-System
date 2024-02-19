7bdc7fec97f3961f4ecfc1ff80a23f2e4247c3b4
CSparrow-22 <152895548+CSparrow-22@users.noreply.github.com>
2024-02-19 07:29:06 +0000

    Add files via upload

--- teamleaderdash.js
+++ teamleaderdash.js
@@ -483,8 +483,7 @@ return true;
 
 function addnewtask() {
 
-event.preventDefault();
-alert("hi");
+// event.preventDefault();
 
 var newtaskID = document.getElementById("new_taskID").value;
 var newtaskName = document.getElementById("new_taskname").value;
@@ -501,7 +500,7 @@ for (var i = 0; i < selectedOptions.length; i++) {
 }
 
 
-
+alert(selectedProjectID);
 if(isValidDateString(newtaskDeadline)){
 $.ajax({
   type: "POST",
