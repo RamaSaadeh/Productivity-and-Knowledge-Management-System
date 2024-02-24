b4fb8a532cdde2f01f8f3959a67a74ad2eac2a81
mjtranter <mjtranter2003@outlook.com>
2024-02-24 16:02:54 +0000

    added deleted functionality

--- delete-user.php
+++ delete-user.php
@@ -18,36 +18,43 @@ $user_id = intval($_POST['user_id']);
 $sql = "UPDATE Posts SET UserID = 0 WHERE UserID = ?;";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("i", $user_id);
+$stmt->execute()
 
 //PostLikes
 $sql = "UPDATE PostLikes SET UserID = 0 WHERE UserID = ?;";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("i", $user_id);
+$stmt->execute()
 
 //Comments
 $sql = "UPDATE Comments SET UserID = 0 WHERE UserID = ?;";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("i", $user_id);
+$stmt->execute()
 
 //CommentLikes
 $sql = "UPDATE CommentLikes SET UserID = 0 WHERE UserID = ?;";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("i", $user_id);
+$stmt->execute()
 
 //projects
 $sql = "UPDATE projects SET leader_id = 0 WHERE leader_id = ?;";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("i", $user_id);
+$stmt->execute()
 
 //project_staff
 $sql = "DELETE FROM project_staff WHERE user_id = ?;";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("i", $user_id);
+$stmt->execute()
 
 //task_staff
 $sql = "DELETE FROM task_staff WHERE user_id = ?;";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("i", $user_id);
+$stmt->execute()
 
 
 // Prepare SQL statement
